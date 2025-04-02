482fed9c7667a32477f8d3ce37eaead6d0a17a53
jojojoker893 <jojojoker114514@gmail.com>
2025-04-02 21:41:27 +0900

    add: migrate実行

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,10 @@
 class Api::V1::UsersController < ApplicationController
 
   def create
-
+    user = User.new(user_params)
+    if user.save
+      render json:{ token: create_token(user.id), status: created}
+    end
   end
 
 end
