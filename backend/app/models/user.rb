8529e1c73271e32dd3d8024b31f56f849dc59637
jojojoker893 <jojojoker114514@gmail.com>
2025-04-03 12:50:22 +0900

    add: usermodel Validation記述

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :password, presence: true, length: { minimum: 6 }
 end
