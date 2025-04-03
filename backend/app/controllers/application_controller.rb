9769ac1a35632d2df8f1c4c28211d62d49be900c
jojojoker893 <jojojoker114514@gmail.com>
2025-04-03 12:39:20 +0900

    add: JWT認証を実装

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,5 +1,28 @@
-class ApplicationController < ActionController::Base
-  def index
-    render json: { message: "Success Health Check!" }, status: :ok    
+class ApplicationController < ActionController::API
+
+  def authenticate_user(password)
+    authorization_header = request.headers[:Authorization]
+    unless authorization_header
+      return render_unauthorized("ログインが必要です")
+    end
+
+    token = authorization_header
+    secret_key = Rails.application.credentials.secret_key_base
+
+    begin
+      decoded_token = JWT.decode(token, secret_key)[0]
+      @current_user = User.find(decoded_token["user_id"])
+    rescue ActiveRecord::RecordNotFound
+      render_unauthorized("ユーザが見つかりません。")
+    rescue JWT::DecodeError => e
+      render_unauthorized("トークンエラー: #{e.message}")
+    end
   end
+
+  def create_token(user_id)
+    payload = { user_id: user_id }
+    secret_key = Rails.application.credentials.secret_key_base
+    JWT.encode(payload, secret_key)
+  end
+
 end
