05cdc2ed781df58dc213c1bec6bee8df2610550a
jojojoker893 <jojojoker114514@gmail.com>
2025-04-11 14:37:44 +0900

    add: modalと使用画像の追加

--- frontend/app/components/Modal.jsx
+++ frontend/app/components/Modal.jsx
@@ -1,11 +1,11 @@
-import canselImage from './pubilc/cansel.svg'
+import cansel from '../../public/cancel.svg'
 
 const Modal = ({ children, onBack }) => {
   return (
     <div className='fixed inset-0 z-50 flex items-center justify-center bg-back bg-opacity-50'>
       <div className='bg-white rounded-lg shadow-lg p-6 w-full max-w-lg relative '>
         <img 
-          src={canselImage} 
+          src={cansel} 
           alt="cansel"
           className='abusolute top-1 right-1 cursor-pointer hover:bg-gray-100 rounded p-1 transition duration-700'
           onClick={()=>{onBack()}}
