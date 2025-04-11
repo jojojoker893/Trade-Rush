594506ee7dab634d2da7cc2ac687e403badb33a6
jojojoker893 <jojojoker114514@gmail.com>
2025-04-11 22:49:26 +0900

    feat: TradeLog保存APIの作成

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get "trade_logs/create"
   namespace :api do
     namespace :v1 do
       get '/trade', to: 'users#capital'
+      resources :trade_logs, only: [:create]
       resources :users, only: [:create]
       post "/login", to: "users#login"
     end
